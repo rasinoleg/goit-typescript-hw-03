3a7cb3415a00103d1bbe3a54b81bb2b1767be9c8
rasinoleg <rasinoleg3@gmail.com>
2023-10-10 15:09:16 +0300

    typescript 03

--- src/2.ts
+++ src/2.ts
@@ -16,7 +16,7 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
+  public name: string;
   department: string;
   salary: number;
 
@@ -32,7 +32,9 @@ class Employee {
 }
 
 class Manager extends Employee {
-  // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary +10000);
+  }
 }
 
 
--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+private signature: number;
+
+constructor() {
+    this.signature = Math.random();
+}
+getSignature(): number {
+    return this.signature;
+}
+}
+
+class Person {
+    private key: Key;
+    
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key
+    }
+}
+
+abstract class House {
+    door: boolean = false;
+    key: Key;
+    tenants: Person[] = [];
+    
+    constructor(key: Key) {
+        this.key = key;
+    }
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (this.door) {
+            this.tenants.push(person);
+            console.log(`Welcome, ${person.getKey().getSignature()}!`);
+        }else {
+            console.log("The door is closed.");
+        }
+    }
+}
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if(key.getSignature() === this.key.getSignature()) {
+            this.door = true;
+            console.log("The door is open.");
+        }else {
+            console.log("Invalid key. The door remains closed.");
+        }
+    }
+}
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
